bcbf400c7f7ddd19c4c7517dcf97ed8af1b6d58c
Yingtian Shi <shiyt0313@gmail.com>
2023-11-08 17:04:16 +0800

    init pages

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,21 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>GenieWizard Developer Study</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/shiyt0313/ReactGenieDeveloperStudy',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  docsRepositoryBase: 'https://github.com/shiyt0313/ReactGenieDeveloperStudy',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'GenieWizard for Developer',
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – ReactGenieDEV'
+    }
   },
 }
 
